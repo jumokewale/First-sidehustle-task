60461bf6598192366c336e37a82549aa1cc73845
Olajumoke Olawale <51835853+jumokewale@users.noreply.github.com>
2021-02-19 21:46:19 +0100

    Update sidehustle.php

--- sidehustle.php
+++ sidehustle.php
@@ -1,9 +1,9 @@
 <?php
 
 
-    for ($x=1; $x<=250; $x++) {
+    for ($x=1; $x<=rand(200, 300); $x++) {              //this loop runs for a number (between 200 to 300) times
         echo "Card ". $x .":       ".  rand(1000000000000, 2000000000000);
-        echo "<br/>";
+        echo "<br/>";                                   //creates  a new line
     }
 
 ?>
